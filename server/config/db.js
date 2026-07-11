510b022987ae7d38ddacecf3f51e3de3ab5c193e
Chintala-Keerthana <keerthiramana.44@gmail.com>
2026-07-11 20:47:45 +0530

    Add TiDB CA certificate

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
 const mysql = require("mysql2");
+const fs = require("fs");
+const path = require("path");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -7,12 +9,12 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: {
+    ca: fs.readFileSync(path.join(__dirname, "ca.pem")),
     minVersion: "TLSv1.2",
     rejectUnauthorized: true,
   },
 });
 
-// Test database connection
 pool.getConnection((err, connection) => {
   if (err) {
     console.error("❌ Database Connection Pool Failed:", err.message);
--- server/config/initDb.js
+++ server/config/initDb.js
@@ -8,10 +8,16 @@ async function initDb() {
   console.log("DB_USER:", process.env.DB_USER);
 
   const connection = await mysql.createConnection({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "Keerthi@123",
-  });
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    minVersion: "TLSv1.2",
+    rejectUnauthorized: true,
+  },
+});
 
   console.log("🔌 Connected to MySQL server successfully.");
 
