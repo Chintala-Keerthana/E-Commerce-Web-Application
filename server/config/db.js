c816af1687013f7ebf1f72d3dba8921de2d542ea
Chintala-Keerthana <keerthiramana.44@gmail.com>
2026-07-11 20:30:07 +0530

    Enable SSL for TiDB Cloud

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,15 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "Keerthi@123",
-  database: process.env.DB_NAME || "ecommerce_db",
-  waitForConnections: true,
-  connectionLimit: 10,
-  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-  queueLimit: 0,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    minVersion: "TLSv1.2",
+    rejectUnauthorized: true,
+  },
 });
 
 // Test database connection
